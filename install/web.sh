19c8b27cd7bc898ca2ab71938bd8d77e9c5fdad8
Bernt Christian EgeÃland <cbegelan@frisurf.no>
2017-10-07 23:34:53 +0000

    Minor Changes

--- install/web.sh
+++ install/web.sh
@@ -10,42 +10,42 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 Basefolder="$(cd ../; pwd)" 
 
 # WEB SERVICES INSTALLATION
-# echo "Installing Node & NPM"
-#    if is_pione; then
+echo "Installing Node & NPM"
+   if is_pione; then
     
-#       wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.6/install.sh | bash
-#       export NVM_DIR="$HOME/.nvm"
-#       [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-#       nvm install node
+      wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.6/install.sh | bash
+      export NVM_DIR="$HOME/.nvm"
+      [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+      nvm install node
 
-#    elif is_pione_w; then
+   elif is_pione_w; then
 
-#       wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.6/install.sh | bash
-#       export NVM_DIR="$HOME/.nvm"
-#       [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-#       nvm install node
+      wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.6/install.sh | bash
+      export NVM_DIR="$HOME/.nvm"
+      [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+      nvm install node
 
-#    elif is_pitwo; then
+   elif is_pitwo; then
 
-#       curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-#       sudo apt-get install -y nodejs 
-#    elif is_pithree; then
+      curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+      sudo apt-get install -y nodejs 
+   elif is_pithree; then
 
-#       curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-#       sudo apt-get install -y nodejs 
-#    else
-#       echo 0
-#    fi
+      curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+      sudo apt-get install -y nodejs 
+   else
+      echo 0
+   fi
 
 
 cd $Basefolder/web
 
-#install pm2 web server
+# install pm2 web server
 sudo npm install --production
-# sudo npm install pm2@latest -g
-# sudo pm2 start process.json --env production
-# sudo pm2 startup
-# sudo pm2 save
+sudo npm install pm2@latest -g
+sudo pm2 start process.json --env production
+sudo pm2 startup
+sudo pm2 save
 
 
 
