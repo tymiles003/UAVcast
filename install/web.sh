895dd75e3fa7ebd5b6f4753e2902191cbb56117f
Bernt Christian Egeland <cbegelan@frisurf.no>
2017-10-09 14:38:46 +0000

    changed node source

--- install/web.sh
+++ install/web.sh
@@ -13,26 +13,26 @@ Basefolder="$(cd ../; pwd)"
 echo "Installing Node & NPM"
    if is_pione; then
       echo "Pi 1"
-      wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.6/install.sh | bash
-      export NVM_DIR="$HOME/.nvm"
-      [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-      nvm install node
+      wget -O - https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v6.11.3.sh | bash
+      # Add support for node CLI tools
+      export PATH=$PATH:/opt/nodejs/bin
 
    elif is_pione_w; then
       echo "Pi0w"
-      wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.6/install.sh | bash
-      export NVM_DIR="$HOME/.nvm"
-      [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-      nvm install node
+      wget -O - https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v6.11.3.sh | bash
+      # Add support for node CLI tools
+      export PATH=$PATH:/opt/nodejs/bin
 
    elif is_pitwo; then
       echo "Pi 2"
       curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
       sudo apt-get install -y nodejs 
+
    elif is_pithree; then
       echo "Pi 3"
       curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
       sudo apt-get install -y nodejs 
+      
    else
       echo 0
    fi
@@ -41,8 +41,8 @@ echo "Installing Node & NPM"
 cd $Basefolder/web
 
 # install pm2 web server
-sudo npm install --production
 sudo npm install pm2@latest -g
+sudo npm install --production
 sudo pm2 start process.json --env production
 sudo pm2 startup
 sudo pm2 save
