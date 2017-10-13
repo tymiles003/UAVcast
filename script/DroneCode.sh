22dc6fca927e1184ee95560923d8ea01628336c6
Bernt Christian Egeland <cbegelan@frisurf.no>
2017-10-13 02:43:43 +0000

    Minor Changes

--- script/DroneCode.sh
+++ script/DroneCode.sh
@@ -3,27 +3,14 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 CONF=$DIR/../DroneConfig.txt
 function DroneInit {
 case $(jq -r '.GSM_Connect' $CONF) in
-	"Ethernet")
-	 ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
-	 echo "Ethernet ip is $ip"
-	 StartBroadcast
-	;;
-	"uqmi")
-		 if ps ax | grep -v grep | grep $(jq -r '.GSM_Connect' $CONF) > /dev/null
-			then
-				echo "$(jq -r '.GSM_Connect' $CONF) service running"
-				StartBroadcast
-			else
-				echo "$(jq -r '.GSM_Connect' $CONF) is not running"
-				echo "Trying to start UQMI"
-				uqmi
-				sleep 5
-				StartBroadcast
-		 fi
-	;;
-	"wvdial")	
-			wvdial
+	"Yes")	
+			ModemManager
 			sleep 5
+			StartBroadcast
+	;;
+	"No")
+			ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
+			echo "Ethernet ip is $ip"
 			StartBroadcast
 	;;
 esac
@@ -63,12 +50,14 @@ sudo ping -c 1 google.com
 done
 echo "If you see this message, then Network was successfully loaded."
 }
-
+function ModemManager {
+    sudo $DIR/./ModemManager.sh > $DIR/../log/ModemManager.log 2>&1
+}
 function Telemetry_Type {
 if [ $(jq -r '.Telemetry_Type' $CONF) == "ttl" ]; then
 pidof udp_redirect >/dev/null
     	if [[ $? -ne 0 ]] ; then  
-			sudo $DIR/./udp-send.sh > $DIR/../log/BroadCast.log 2>&1 & 
+			sudo $DIR/./udp-send.sh > $DIR/../log/TTLRedirect.log 2>&1 & 
 			sleep 0.3
 			pidof udp_redirect >/dev/null
 			sleep 0.3
@@ -82,28 +71,10 @@ pidof udp_redirect >/dev/null
 		else
 	    echo "Another udp_redirect process already running"
         fi
-# elif [ $(jq -r '.Telemetry_Type' $CONF) == "gpio" ]; then
-# 		pidof ser2net >/dev/null
-#     	if [[ $? -ne 0 ]] ; then  
-# 			sudo $DIR/./ser2net.sh > $DIR/../log/BroadCast-GPIO.log 2>&1 & 
-# 			sleep 0.3
-# 			pidof ser2net >/dev/null
-# 			sleep 0.3
-# 				if [[ $? -eq 0 ]] ; then 
-# 					echo 'ser2net script started'
-# 					return 1
-# 					else
-# 					echo 'Another ser2net process already running'
-# 				fi
-# 				return 0
-# 		else
-# 	    echo "Another ser2net process already running"
-#        fi
-# fi
 elif [ $(jq -r '.Telemetry_Type' $CONF) == "gpio" ]; then
 		pidof cmavnode >/dev/null
     	if [[ $? -ne 0 ]] ; then  
-			sudo $DIR/./cmavnode.sh > $DIR/../log/BroadCast-GPIO.log 2>&1 &
+			sudo $DIR/./cmavnode.sh &
 			sleep 0.3
 			pidof cmavnode >/dev/null
 			sleep 0.3
@@ -136,64 +107,6 @@ pidof gst-launch-1.0 >/dev/null
 	fi
 fi
 }
-
-function uqmi {
-sudo uqmi -d /dev/cdc-wdm0 --stop-network 4294967295 --autoconnect
-sleep 2
-    if ! sudo uqmi -s -d /dev/cdc-wdm0 --get-data-status | grep '"connected"' > /dev/null; then
-		sudo uqmi -d /dev/cdc-wdm0 --stop-network 4294967295 --autoconnect
-		sleep 2
-		sudo uqmi -d /dev/cdc-wdm0 --network-register
-		echo network register
-		sleep 3
-		echo Connecting 4G
-			sudo uqmi -s -d /dev/cdc-wdm0 --start-network $(jq -r '.APN_name' $CONF) --keep-client-id wds --autoconnect &
-		sleep 15
-		if ! sudo uqmi -s -d /dev/cdc-wdm0 --get-data-status | grep '"connected"' > /dev/null; then
-			echo "GSM Not Connected!"
-		else
-			echo "GSM Connected"
-			sudo dhclient -v wwan0
-	fi
-	else
-		echo "UQMI is already connected to internet."
-	fi
-	}
-function wvdial {
-FILE="$DIR/./wvdial.conf"
-/bin/cat <<EOM >$FILE
-[Dialer Defaults]
-Init1 = $(jq -r '.wv_init1' $CONF)
-Init2 = $(jq -r '.wv_init2' $CONF)
-Init3 = $(jq -r '.wv_init3' $CONF) ,"IP", "$(jq -r '.APN_name' $CONF)"
-Stupid Mode = 1 
-Carrier Check = $(jq -r '.wv_carrier_check' $CONF)
-MessageEndPoint = "0x01"
-Modem Type = Analog Modem
-ISDN = 0
-Phone = $(jq -r '.wv_Phone' $CONF)
-Modem = $(jq -r '.wv_Modem' $CONF)
-Username = $(jq -r '.wv_Username' $CONF)
-Password = $(jq -r '.wv_Password' $CONF)
-Baud = $(jq -r '.wv_Baud' $CONF)
-Auto Reconnect = $(jq -r '.wv_Auto_Reconnect' $CONF)
-EOM
-   if ps ax | grep -v grep | grep $(jq -r '.GSM_Connect' $CONF) > /dev/null
-			then
-				echo "$(jq -r '.GSM_Connect' $CONF) service running"
-			else
-				echo "$(jq -r '.GSM_Connect' $CONF) not running. We will start it"
-				sudo wvdial -C $DIR/./wvdial.conf > $DIR/../log/wvdial.log 2>&1 & 
-				sleep 20
-				  if ps ax | grep -v grep | grep $(jq -r '.GSM_Connect' $CONF) > /dev/null
-				      then
-				      echo "$(jq -r '.GSM_Connect' $CONF) successfully started"
-					  rm $FILE
-				  else 
-				      echo "could not start $(jq -r '.GSM_Connect' $CONF)"
-			      fi
-	 fi
-	}
 function inadyn {
 if [ $(jq -r '.UseDns' $CONF) == "Yes" ]; then
    sleep 1
